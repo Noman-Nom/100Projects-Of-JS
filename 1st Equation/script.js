1cf049bbc8d06641f694e405398cf747cacae2e4
Noman-Nom <noman5456318@gmail.com>
2023-11-26 17:07:19 +0500

    js working

--- 1st Equation/script.js
+++ 1st Equation/script.js
@@ -1,1 +1,17 @@
-console.log("hello")
+const form = document.querySelector('form')
+// console.log(form)
+
+form.addEventListener('submit', (e)=>{
+
+    e.preventDefault()
+
+const vi = document.querySelector('#vi').value
+const a = document.querySelector('#a').value
+const t = document.querySelector('#t').value
+
+console.log(vi)
+console.log(a)
+console.log(t)
+
+
+})
