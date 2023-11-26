fa71a520ca04b1485e8b87f6dd8996bb2ac8afe3
Noman-Nom <noman5456318@gmail.com>
2023-11-26 15:40:21 +0500

    changes

--- To-Do-List/script.js
+++ To-Do-List/script.js
@@ -1,32 +1,49 @@
-const input = document.querySelector("#input-box")
-const list = document.querySelector('#list-container')
+const input = document.querySelector("#input-box");
+const list = document.querySelector('#list-container');
+const btn = document.querySelector("#addTask");
 
-const btn = document.querySelector("#addTask")
 
-const addTask=()=>{
-    if(input.value===""){
-        alert("please write some task...")
+const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+
+savedTasks.forEach(task => {
+    addTaskToDOM(task);
+});
+
+function addTask() {
+    if (input.value === "") {
+        alert("Please write some task...");
+    } else {
+        const taskText = input.value;
+        addTaskToDOM(taskText);
+
+       
+        savedTasks.push(taskText);
+        localStorage.setItem('tasks', JSON.stringify(savedTasks));
+
+        input.value = "";
     }
-    else{
-        let li = document.createElement("li")
-        li.innerHTML = input.value
-        list.appendChild(li);
-
-        let span = document.createElement("span")
-        span.innerHTML = "\u00d7" 
-        li.appendChild(span)
-
-    }
-    input.value = ""
 }
 
+function addTaskToDOM(taskText) {
+    let li = document.createElement("li");
+    li.innerHTML = taskText;
+    list.appendChild(li);
 
-list.addEventListener('click', (e)=>{
-        // console.log(e.target)
-            if(e.target.tagName==="LI"){
-                e.target.classList.toggle("checked")
-            }
-            else if(e.target.tagName==="SPAN"){
-                e.target.parentElement.remove()
-            }
-})
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+}
+
+list.addEventListener('click', (e) => {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+        const removedTask = e.target.parentElement.textContent.trim();
+        e.target.parentElement.remove();
+
+    
+        const updatedTasks = savedTasks.filter(task => task !== removedTask);
+        localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+    }
+});
--- projectRandomclrChanger/script.js
+++ projectRandomclrChanger/script.js
@@ -44,7 +44,7 @@ const randomColor=()=>{
     const hex = "0123456789ABCDEF"
     let  color = "#"
     for (let i = 0; i <6 ; i++) {
-        color += hex[Math.floor(Math.random()*9)]
+        color += hex[Math.floor(Math.random()*hex.length)]
         
     }
     console.log(color)
